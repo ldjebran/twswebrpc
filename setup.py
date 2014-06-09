412bb7921ed4497b15d9d2f3a70c9110d82ec0de
Lezzoum Djebran <ldjebran@gmail.com>
2014-06-09 09:23:14 +0300

    added required packages

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+
+from setuptools import setup
 
 setup(
     name='twswebrpc',
-    version='0.1',
+    version='0.1.0',
     packages=['twswebrpc'],
     url='https://github.com/ldjebran/twswebrpc',
-    license='Apache',
+    license='Apache Version 2.0',
     author='Djebran Lezzoum',
     author_email='ldjebran@gmail.com',
-    description='Twistedmatrix web JSON RPC Client and Resource  '
+    description='Twisted web JSON RPC Client and Resource',
+    install_requires=['zope.interface', 'twisted']
 )
