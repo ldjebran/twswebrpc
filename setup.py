b6e0252e2a42fad503c758be52d1f31b684f75b7
Lezzoum Djebran <ldjebran@gmail.com>
2014-06-09 11:00:07 +0300

    updated some parameters

--- setup.py
+++ setup.py
@@ -4,11 +4,17 @@ from setuptools import setup
 setup(
     name='twswebrpc',
     version='0.1.0',
+    platforms=['any'],
     packages=['twswebrpc'],
+    zip_safe=True,
     url='https://github.com/ldjebran/twswebrpc',
-    license='Apache Version 2.0',
+    license='Apache',
     author='Djebran Lezzoum',
     author_email='ldjebran@gmail.com',
-    description='Twisted web JSON RPC Client and Resource',
-    install_requires=['zope.interface', 'twisted']
+    description='Twisted JSON RPC web Client and web Resource',
+    keywords='twisted rpc web json',
+    install_requires=['zope.interface', 'twisted'],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+    ],
 )
